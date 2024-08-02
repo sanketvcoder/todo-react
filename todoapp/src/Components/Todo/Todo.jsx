4dd9d8a75178d0db9061bb97d3dc0cbd9406382a
Sanket Verma <sanketv403@gmail.com>
2024-08-02 16:18:01 +0530

    update on 2nd aug 2024

--- todoapp/src/Components/Todo/Todo.jsx
+++ todoapp/src/Components/Todo/Todo.jsx
@@ -1,14 +1,46 @@
-import React ,{useState} from 'react'
-import './Todo.css'
-export default function Todo() {
-    const [todo, setValue] = useState("")
-  return (
-    <div className = "main">
-        <div className="input-btn">
-            <input className='input-bx' value = {todo} type="text" />
-            <button className = 'btn-bx'>Submit</button>
+import React, { useState } from 'react';
+import './Todo.css';
 
+export default function Todo() {
+    const [todo, setTodo] = useState(""); // State for the input value
+    const [arr, setArrayObj] = useState([]); // State for the array of todos
+    function handleData() {
+        // Create a new object with the todo value
+        const newObj = { input: todo };
+
+        // Create a new array by adding the new object
+        const newArray = [...arr, newObj];
+
+        // Update the state with the new array
+        setArrayObj(newArray);
+
+        // Log the updated array
+        console.log(newArray);
+
+        // Clear the input field
+        setTodo("");
+    }
+
+    return (
+        <div className="main">
+            <div className="first-box">
+                <div className="input-btn">
+                    <input
+                        onChange={(e) => setTodo(e.target.value)}
+                        className='input-bx'
+                        value={todo}
+                        type="text"
+                    />
+                    <button onClick={handleData} className='btn-bx'>Submit</button>
+                </div>
+            </div>
+            <div className="sec-box">
+                <ul>
+                    {arr.map((item, index) => (
+                        <li key={index}>{item.input}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
